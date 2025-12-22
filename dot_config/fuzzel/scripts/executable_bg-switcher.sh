8946b2c70b634284dc46e271c8c2e05b0a9964a4
torifork <jonass.main@proton.me>
2025-12-22 22:16:14 +0200

    clipboard is now hidden, fixed gtk theming, changed fuzzel and mako theming, fixed up some other stuff

--- dot_config/fuzzel/scripts/executable_bg-switcher.sh
+++ dot_config/fuzzel/scripts/executable_bg-switcher.sh
@@ -5,7 +5,7 @@ WALLDIR="$HOME/Pictures/Wallpapers"
 choice=$(find "$WALLDIR" -type f \( -iname "*.*" \) \
   | sort \
   | sed "s|^$WALLDIR/||" \
-  | fuzzel --dmenu --prompt="Wallpaper: ")
+  | fuzzel --dmenu --minimal-lines --prompt="Wallpaper: ")
 
 [ -z "$choice" ] && exit 0
 
@@ -13,5 +13,5 @@ swww img "$WALLDIR/$choice" \
 	--transition-type any \
 	--transition-duration 2 \
 	--transition-fps 60
-matugen --type scheme-neutral image "$WALLDIR/$choice"
+matugen --type scheme-tonal-spot image "$WALLDIR/$choice"
 ln -sf "$WALLDIR/$choice" "$HOME/.cache/lockscreen.png"
